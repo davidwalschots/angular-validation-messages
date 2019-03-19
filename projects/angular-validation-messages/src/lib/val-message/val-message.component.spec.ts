f9e1d108f6c107d076c8568e81766945dfaf1445
David Walschots <--global>
2019-03-19 15:50:07 +0100

    Add canShow specs

--- projects/angular-validation-messages/src/lib/val-message/val-message.component.spec.ts
+++ projects/angular-validation-messages/src/lib/val-message/val-message.component.spec.ts
@@ -3,47 +3,43 @@ import { ValMessageComponent } from './val-message.component';
 import { Component, ViewChild } from '@angular/core';
 
 describe('ValMessageComponent', () => {
-  @Component({
-    template: `
-      <val-message #withoutDefault></val-message>
-      <val-message #withDefaultWithoutValue default></val-message>
-      <val-message #withDefaultWithFalseValueAsString default="false"></val-message>
-      <val-message #withDefaultWithFalseValueAsBoolean [default]="false"></val-message>
-      <val-message #withDefaultWithTrueValueAsString default="true"></val-message>
-      <val-message #withDefaultWithTrueValueAsBoolean [default]="true"></val-message>
-      <val-message #withDefaultWithSomeValue default="bladiebla"></val-message>
-    `
-  })
-  class TestHostComponent {
-    @ViewChild('withoutDefault') withoutDefault: ValMessageComponent;
-    @ViewChild('withDefaultWithoutValue') withDefaultWithoutValue: ValMessageComponent;
-    @ViewChild('withDefaultWithFalseValueAsString') withDefaultWithFalseValueAsString: ValMessageComponent;
-    @ViewChild('withDefaultWithFalseValueAsBoolean') withDefaultWithFalseValueAsBoolean: ValMessageComponent;
-    @ViewChild('withDefaultWithTrueValueAsString') withDefaultWithTrueValueAsString: ValMessageComponent;
-    @ViewChild('withDefaultWithTrueValueAsBoolean') withDefaultWithTrueValueAsBoolean: ValMessageComponent;
-    @ViewChild('withDefaultWithSomeValue') withDefaultWithSomeValue: ValMessageComponent;
-  }
-
-  let component: TestHostComponent;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ TestHostComponent, ValMessageComponent ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    const fixture = TestBed.createComponent(TestHostComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-
   describe(`has a 'default' property, which when`, () => {
+    @Component({
+      template: `
+        <val-message #withoutDefault></val-message>
+        <val-message #withDefaultWithoutValue default></val-message>
+        <val-message #withDefaultWithFalseValueAsString default="false"></val-message>
+        <val-message #withDefaultWithFalseValueAsBoolean [default]="false"></val-message>
+        <val-message #withDefaultWithTrueValueAsString default="true"></val-message>
+        <val-message #withDefaultWithTrueValueAsBoolean [default]="true"></val-message>
+        <val-message #withDefaultWithSomeValue default="bladiebla"></val-message>
+      `
+    })
+    class TestHostComponent {
+      @ViewChild('withoutDefault') withoutDefault: ValMessageComponent;
+      @ViewChild('withDefaultWithoutValue') withDefaultWithoutValue: ValMessageComponent;
+      @ViewChild('withDefaultWithFalseValueAsString') withDefaultWithFalseValueAsString: ValMessageComponent;
+      @ViewChild('withDefaultWithFalseValueAsBoolean') withDefaultWithFalseValueAsBoolean: ValMessageComponent;
+      @ViewChild('withDefaultWithTrueValueAsString') withDefaultWithTrueValueAsString: ValMessageComponent;
+      @ViewChild('withDefaultWithTrueValueAsBoolean') withDefaultWithTrueValueAsBoolean: ValMessageComponent;
+      @ViewChild('withDefaultWithSomeValue') withDefaultWithSomeValue: ValMessageComponent;
+    }
+  
+    let component: TestHostComponent;
+  
+    beforeEach(async(() => {
+      TestBed.configureTestingModule({
+        declarations: [ TestHostComponent, ValMessageComponent ]
+      })
+      .compileComponents();
+    }));
+  
+    beforeEach(() => {
+      const fixture = TestBed.createComponent(TestHostComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+
     it('not declared, makes the component not default', () => {
       expect(component.withoutDefault.default).toEqual(false);
     });
@@ -62,19 +58,41 @@ describe('ValMessageComponent', () => {
       expect(component.withDefaultWithTrueValueAsString.default).toEqual(true);
       expect(component.withDefaultWithSomeValue.default).toEqual(true);
     });
+  });
 
-    describe(`can show itself when`, () => {
-      it(`there is an error with a key equal to the 'for' attribute`, () => {
-
-      });
-
-      it(`it is a default component`, () => {
-
-      });
+  describe(`canShow`, () => {
+    let component: ValMessageComponent;
+  
+    beforeEach(async(() => {
+      TestBed.configureTestingModule({
+        declarations: [ ValMessageComponent ]
+      })
+      .compileComponents();
+    }));
+  
+    beforeEach(() => {
+      const fixture = TestBed.createComponent(ValMessageComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
     });
 
-    it(`cannot show itself when it isn't default, nor a matching key is provided`, () => {
+    it(`returns true when there is an error with a key equal to the 'for' attribute`, () => {
+      component.for = 'required';
 
+      expect(component.canShow({ minlength: true, required: true })).toEqual(true);
+    });
+
+    it(`returns true when it is a 'default' component`, () => {
+      component.default = true;
+
+      expect(component.canShow({ minlength: true, required: true })).toEqual(true);
+    });
+
+    it(`returns false when there is no key equal to the 'for' attribute, and it isn't default`, () => {
+      component.default = false;
+      component.for = 'maxlength';
+
+      expect(component.canShow({ minlength: true, required: true })).toEqual(false);
     });
   });
 });
