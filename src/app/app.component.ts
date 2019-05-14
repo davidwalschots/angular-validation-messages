2d659b9bb92c03b4f136daf3ce697e3e1e95ee53
David Walschots <--global>
2019-05-14 11:24:19 +0200

    Simple form to do some testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { formArrayNameProvider } from '@angular/forms/src/directives/reactive_directives/form_group_name';
 
 @Component({
   selector: 'avmd-root',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private fb: FormBuilder) {
+  }
+
   title = 'angular-validation-messages-demo';
+
+  form = this.fb.group({
+    name: this.fb.group({
+      firstName: ['', [Validators.required,
+        Validators.minLength(10),
+        Validators.maxLength(5)]],
+      middleName: ['', [Validators.maxLength(50)]],
+      lastName: ['', [Validators.required,
+        Validators.maxLength(10 * 5)]]
+    }),
+    age: [null, [
+      Validators.required,
+      Validators.min(0),
+      Validators.max(150)
+    ]]
+  });
 }
