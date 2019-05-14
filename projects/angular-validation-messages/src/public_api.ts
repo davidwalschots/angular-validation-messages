fe21e9762378623bd14807e3d11b9367cf078a79
David Walschots <--global>
2019-05-14 11:10:38 +0200

    Fix project build

--- projects/angular-validation-messages/src/public_api.ts
+++ projects/angular-validation-messages/src/public_api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of angular-validation-messages
  */
 
-export * from './lib/angular-validation-messages.service';
 export * from './lib/val-messages/val-messages.component';
 export * from './lib/angular-validation-messages.module';
